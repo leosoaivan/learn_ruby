396b9eb60362e5bd21b65faddbe115ba525ab4e5
Leonard Soai-Van <leo.soaivan@gmail.com>
2016-10-20 05:01:15 -0400

    Add solution 05

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,21 @@
 class Book
-# write your code here
+
+  def title
+    @title
+  end
+
+  def title=(str)
+    conjunctions = %w[and or but]
+    prepositions = %w[in of the a an]
+
+    ary = str.split
+
+    ary.collect do |x|
+      x.gsub!(/^\w+/, &:capitalize) unless conjunctions.include?(x) || prepositions.include?(x)
+    end
+
+    ary[0].capitalize!
+    @title = ary.join(" ")
+  end
+
 end
